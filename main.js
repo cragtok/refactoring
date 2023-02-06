db6b9bacc18218ec10674a3d86c831b060e09216
cragtok <lidfun@protonmail.com>
2023-02-07 05:36:01 +0600

    Extract charge calculation for one performance into amountFor() function

--- main.js
+++ main.js
@@ -2,16 +2,7 @@ import invoices from "./invoices.json" assert { type: "json" };
 import plays from "./plays.json" assert { type: "json" };
 
 function statement(invoice, plays) {
-    let totalAmount = 0;
-    let volumeCredits = 0;
-    let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-        minimumFractionDigits: 2,
-    }).format;
-    for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
+    function amountFor(perf, play) {
         let thisAmount = 0;
         switch (play.type) {
             case "tragedy":
@@ -30,6 +21,19 @@ function statement(invoice, plays) {
             default:
                 throw new Error(`unknown type: ${play.type}`);
         }
+        return thisAmount;
+    }
+    let totalAmount = 0;
+    let volumeCredits = 0;
+    let result = `Statement for ${invoice.customer}\n`;
+    const format = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 2,
+    }).format;
+    for (let perf of invoice.performances) {
+        const play = plays[perf.playID];
+        let thisAmount = amountFor(perf, play);
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
         // add extra credit for every ten comedy attendees
