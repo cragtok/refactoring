efcdfe0df03d7d976622efd4e14e6d4058e8abd4
cragtok <lidfun@protonmail.com>
2023-02-07 07:38:18 +0600

    Create html version of rendering function

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import invoices from "./invoices.json" assert { type: "json" };
 import plays from "./plays.json" assert { type: "json" };
-import statement from "./statement.js";
+import { statement } from "./statement.js";
 
 function main() {
     invoices.forEach((invoice) => console.log(statement(invoice, plays)));
--- statement.js
+++ statement.js
@@ -1,5 +1,13 @@
 import createStatementData from "./createStatementData.js";
 
+function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 2,
+    }).format(aNumber / 100);
+}
+
 function renderPlainText(data, plays) {
     let result = `Statement for ${data.customer}\n`;
 
@@ -11,15 +19,26 @@ function renderPlainText(data, plays) {
     result += `Amount owed is ${usd(data.totalAmount)}\n`;
     result += `You earned ${data.totalVolumeCredits} credits\n`;
     return result;
-
-    function usd(aNumber) {
-        return new Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "USD",
-            minimumFractionDigits: 2,
-        }).format(aNumber / 100);
-    }
 }
-export default function statement(invoice, plays) {
+
+function renderHtml(data) {
+    let result = `<h1>Statement for ${data.customer}</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+    for (let perf of data.performances) {
+        result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+        result += `<td>${usd(perf.amount)}</td></tr>\n`;
+    }
+    result += "</table>\n";
+    result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+    return result;
+}
+
+export function statement(invoice, plays) {
     return renderPlainText(createStatementData(invoice, plays));
+}
+
+export function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
 }
