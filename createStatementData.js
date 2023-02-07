1fc1f4f4e55fa8d538bbd0b3ab196ec85c486b31
cragtok <lidfun@protonmail.com>
2023-02-07 08:00:14 +0600

    Add amount calculation function to TragedyCalculator class

--- createStatementData.js
+++ createStatementData.js
@@ -35,7 +35,15 @@ class PerformanceCalculator {
     }
 }
 
-class TragedyCalculator extends PerformanceCalculator {}
+class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
+        }
+        return result;
+    }
+}
 class ComedyCalculator extends PerformanceCalculator {}
 
 export default function createStatementData(invoice, plays) {
