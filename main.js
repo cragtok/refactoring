417a884c8b2a1ba3b70e9299adb49061c617edfe
cragtok <lidfun@protonmail.com>
2023-02-07 07:35:13 +0600

    Extract statement function into its own file and import it in main file

--- main.js
+++ main.js
@@ -1,30 +1,9 @@
 import invoices from "./invoices.json" assert { type: "json" };
 import plays from "./plays.json" assert { type: "json" };
-import createStatementData from "./createStatementData.js";
+import statement from "./statement.js";
 
-function statement(invoice, plays) {
-    return renderPlainText(createStatementData(invoice, plays));
+function main() {
+    invoices.forEach((invoice) => console.log(statement(invoice, plays)));
 }
 
-function renderPlainText(data, plays) {
-    let result = `Statement for ${data.customer}\n`;
-
-    for (let perf of data.performances) {
-        result += ` ${perf.play.name}: ${usd(perf.amount)} (${
-            perf.audience
-        } seats)\n`;
-    }
-    result += `Amount owed is ${usd(data.totalAmount)}\n`;
-    result += `You earned ${data.totalVolumeCredits} credits\n`;
-    return result;
-
-    function usd(aNumber) {
-        return new Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "USD",
-            minimumFractionDigits: 2,
-        }).format(aNumber / 100);
-    }
-}
-
-invoices.forEach((invoice) => console.log(statement(invoice, plays)));
+main();
