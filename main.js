4aa875008e838ddd1fcbd1208d7a7a53c723f93c
cragtok <lidfun@protonmail.com>
2023-02-07 06:57:16 +0600

    Replace all references to 'playFor' in calculation function to use the data from its 'data' parameter

--- main.js
+++ main.js
@@ -22,12 +22,12 @@ function renderPlainText(data, plays) {
     let result = `Statement for ${data.customer}\n`;
 
     for (let perf of data.performances) {
-        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
+        result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${
             perf.audience
         } seats)\n`;
     }
     result += `Amount owed is ${usd(totalAmount())}\n`;
-    result += `You earned ${getTotalVolumeCredits()} credits\n`;
+    result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
 
     function totalAmount() {
@@ -38,7 +38,7 @@ function renderPlainText(data, plays) {
         return result;
     }
 
-    function getTotalVolumeCredits() {
+    function totalVolumeCredits() {
         let result = 0;
         for (let perf of data.performances) {
             result += volumeCreditsFor(perf);
@@ -48,7 +48,7 @@ function renderPlainText(data, plays) {
 
     function amountFor(aPerformance) {
         let result = 0;
-        switch (playFor(aPerformance).type) {
+        switch (aPerformance.play.type) {
             case "tragedy":
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -63,7 +63,7 @@ function renderPlainText(data, plays) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`unknown type: ${play.type}`);
+                throw new Error(`unknown type: ${aPerformance.play.type}`);
         }
         return result;
     }
@@ -75,7 +75,7 @@ function renderPlainText(data, plays) {
     function volumeCreditsFor(aPerformance) {
         let result = 0;
         result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === playFor(aPerformance).type)
+        if ("comedy" === aPerformance.play.type)
             result += Math.floor(aPerformance.audience / 5);
         return result;
     }
