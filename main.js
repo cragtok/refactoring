08764f0a9a873455474e409ec45b27eb60657e4d
cragtok <lidfun@protonmail.com>
2023-02-07 06:34:30 +0600

    Separate calculation and formatting into separate functions

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ import invoices from "./invoices.json" assert { type: "json" };
 import plays from "./plays.json" assert { type: "json" };
 
 function statement(invoice, plays) {
+    return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
 
     for (let perf of invoice.performances) {
